c370fc435a852a861d4a8fe89038ef94e4efd762
Rutul Patel <rutulpatel5407@gmail.com>
2025-08-28 16:24:19 +0530

    PR1-1,2,3

---
+++ Practical-1/2.sql
@@ -0,0 +1,8 @@
+INSERT INTO ACCOUNT_24012011123 (acc_no, Name, City, Balance, loan_taken) VALUES
+('A001', 'Patel Jigar', 'Mehsana', 50000, 'YES'),
+('A002', 'Patel Ramesh', 'Mehsana', 50000, 'YES'),
+('A003', 'Dave Hardik', 'Ahmedabad', 75000, 'NO'),
+('A004', 'Soni Hetal', 'Ahmedabad', 100000, 'NO'),
+('A005', 'Sony Atul', 'Vadodara', 100000, 'YES');
+
+SELECT * FROM ACCOUNT_24012011123;
---
+++ Practical-1/3.sql
@@ -0,0 +1,9 @@
+CREATE TABLE LOAN_24012011123 (
+    loan_no VARCHAR(5),
+    acc_no VARCHAR(5),
+    loan_amt DECIMAL(10,2),
+    interest_rate DECIMAL(5,2),
+    loan_date DATE,
+    remaining_loan DECIMAL(10,2)
+);
+DESCRIBE LOAN_24012011123;
